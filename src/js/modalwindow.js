b868d82dedab7eecbd1334b933f5f7e938400579
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-02-24 21:48:44 +0100

    1

--- src/js/modalwindow.js
+++ src/js/modalwindow.js
@@ -1,5 +1,102 @@
+import {
+  fetchBookById,
+  showBookById,
+  showCategoryList,
+  showAllCategories,
+} from './render-functions';
+
 const modal = document.querySelector('.backdrop');
 const closeModalWindow = document.querySelector('.close-modal');
+
+Promise.all([showCategoryList(), showAllCategories()]).then(() => {
+  let liGalleryItems = document.querySelectorAll('.gallery-item');
+
+  liGalleryItems.forEach(item => {
+    item.addEventListener('click', onClickGalleryItem);
+  });
+});
+
+async function onClickGalleryItem(e) {
+  let id = e.target.closest('.gallery-item').id;
+
+  const response = await fetchBookById(id);
+  renderModalwindow(response);
+  console.log(response);
+}
+
+async function renderModalwindow(response) {
+  const markup = response
+    .map(
+      ({
+        book_image,
+        author,
+        description,
+        title,
+        amazon_product_url,
+        book_uri,
+      }) => {
+        return `<div class="backdrop close">
+  <div class="modal">
+    <button class="close-modal">
+      <svg class="modal-svg-close" width="24" height="24">
+        <use href="../img/icons.svg#icon-x-close"></use>
+      </svg>
+    </button>
+
+    <div class="desctop">
+      <img src="${book_image}" alt="${title}" class="img-modal" />
+      <div class="lauch">
+        <div class="tittle-books">
+          <h2 class="boock-name">${title}</h2>
+          <p class="author">${author}</p>
+        </div>
+
+        <p class="about-book">
+         ${description}
+        </p>
+
+        <ul class="sale-place">
+          <li>
+            <a href="${amazon_product_url}" target="_blank"
+              ><img
+                class="sale-place-links"
+                src="../img/amazon.png"
+                alt="amazon"
+                width="62"
+                height="19"
+            /></a>
+          </li>
+          <li>
+            <a href="${book_uri}" target="_blank"
+              ><img
+                class="sale-place-links"
+                src="../img/apple.png"
+                alt="amazon"
+                width="33"
+                height="32"
+            /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <button class="add-lokalstorage" type="button">add to shopping list</button>
+    <button class="remove-lokalstorage hiden" type="button">
+      remove from the shopping list
+    </button>
+    <p class="congrat hiden">
+      Сongratulations! You have added the book to the shopping list. To delete,
+      press the button “Remove from the shopping list”.
+    </p>
+  </div>
+</div>
+`;
+      }
+    )
+    .join('');
+
+  return markup;
+}
 
 function closeModal() {
   modal.classList.add('close');
@@ -19,7 +116,7 @@ function modalClickHandler(event) {
 }
 
 function keydownHandler(event) {
-  if (event.key === 'Escape' && !modal.classList.contains('close')) {
+  if (event.key === 'Escape') {
     closeModal();
   }
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,81 +1,92 @@
-import axios from "axios";
+import axios from 'axios';
 
 const categoryList = document.querySelector('.category-list'),
-    galleryList = document.querySelector('.gallery-list'),
-    allCategories = document.querySelector('#all-categories'),
-    categoryName = document.querySelector('.category-title');
+  galleryList = document.querySelector('.gallery-list'),
+  allCategories = document.querySelector('#all-categories'),
+  categoryName = document.querySelector('.category-title');
 
 async function fetchCategories() {
-    const response = await axios.get(`https://books-backend.p.goit.global/books/category-list`);
-    return response;
+  const response = await axios.get(
+    `https://books-backend.p.goit.global/books/category-list`
+  );
+  return response;
 }
 async function fetchTopCategories() {
-    const response = await axios.get('https://books-backend.p.goit.global/books/top-books');
-    return response;
+  const response = await axios.get(
+    'https://books-backend.p.goit.global/books/top-books'
+  );
+  return response;
 }
 async function fetchBooksByCategory(selectedCategory) {
-    const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=${selectedCategory}`);
-    return response;
+  const response = await axios.get(
+    `https://books-backend.p.goit.global/books/category?category=${selectedCategory}`
+  );
+  return response;
 }
-async function fetchBookById(bookId) {
-    const response = await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
-    return response.data;
-}
+export const fetchBookById = async bookId => {
+  const response = await axios.get(
+    `https://books-backend.p.goit.global/books/${bookId}`
+  );
+  return response.data;
+};
 
-async function showBookById() {
-    const bookId = "643282b1e85766588626a0b2";
-    const response = await fetchBookById(bookId);
-    // console.log(response);
-}
-async function showCategoryList() {
-    const response = await fetchCategories();
-    renderCategory(response.data);
+export const showBookById = async e => {
+  let id = e.target.closest('.gallery-item').id;
 
-}
-async function showAllCategories() {
-    const response = await fetchTopCategories();
-    for (let i = 0; i < 4; i++) {
-        renderListGroup(response.data[i]);
-    }
-    // const seeMoreBtn = document.querySelectorAll('.see-more-btn');
-    // seeMoreBtn.forEach(button => {
-    //     button.addEventListener('click', (e) => {
-    //         console.log(e.target.parentNode.firstChild);
-    //         console.log('asasas');
-    //     })
-    // })
-}
+  const response = await fetchBookById(id);
+};
 
-showCategoryList();
-showAllCategories();
+export const showCategoryList = async () => {
+  const response = await fetchCategories();
+  renderCategory(response.data);
+};
+
+export const showAllCategories = async () => {
+  const response = await fetchTopCategories();
+  for (let i = 0; i < 4; i++) {
+    renderListGroup(response.data[i]);
+  }
+};
+
+// const seeMoreBtn = document.querySelectorAll('.see-more-btn');
+// seeMoreBtn.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//         console.log(e.target.parentNode.firstChild);
+//         console.log('asasas');
+//     })
+// })
 
 function renderCategory(data) {
-    const markup = data.map(({ list_name }) => {
-        return `
+  const markup = data
+    .map(({ list_name }) => {
+      return `
       <li class="category-list-item">
 				<h4 class="category-name">${list_name}</h4>
 			</li>
-    `
-    }).join('');
-    categoryList.innerHTML += markup;
+    `;
+    })
+    .join('');
+  categoryList.innerHTML += markup;
 }
 function renderTopCategoriesBooks(data) {
-    const markup = data.map(({ book_image, author, title }) => {
-        return `
-        <li class="gallery-item">
+  const markup = data
+    .map(({ book_image, author, title, _id }) => {
+      return `
+        <li class="gallery-item" id="${_id}">
             <div class="ig-wrapper">
                 <img src="${book_image}" alt="" class="book-cover" />
             </div>
             <h3 class="book-title">${isCorrectTextLength(title)}</h3>
             <h5 class="book-author">${isCorrectTextLength(author)}</h5>
-        </li>`
-    }).join('');
-    return markup;
+        </li>`;
+    })
+    .join('');
+  return markup;
 }
 function renderListGroup(data) {
-    categoryName.textContent = 'Best Sellers Books';
-    const { list_name, books } = data;
-    const markup = `
+  categoryName.textContent = 'Best Sellers Books';
+  const { list_name, books } = data;
+  const markup = `
     <li class="gallery-list-group">
       <h3 class="list-group-name">${list_name}</h3>
         <ul class="gallery-list-item">
@@ -84,47 +95,53 @@ function renderListGroup(data) {
       <button class="see-more-btn">See more</button>
     </li>
   `;
-    galleryList.innerHTML += markup;
+  galleryList.innerHTML += markup;
 }
 function renderListByCategory(data) {
-    categoryName.textContent = data[0].list_name; //беремо назву категорії у першого елемента
-    console.log(data);
-    const markup = data.map(({ book_image, title, author }) => {
-        return `
-        <li class="gallery-item">
+  categoryName.textContent = data[0].list_name; //беремо назву категорії у першого елемента
+  console.log(data);
+  const markup = data
+    .map(({ book_image, title, author, _id }) => {
+      return `
+        <li class="gallery-item" id="${_id}">
           <img src="${book_image}" alt="${title}" class="book-cover" />
           <h3 class="book-title">${isCorrectTextLength(title)}</h3>
           <h5 class="book-author">${isCorrectTextLength(author)}</h5>
-        </li>`
-    }).join('');
-    galleryList.innerHTML = markup;
+        </li>`;
+    })
+    .join('');
+  galleryList.innerHTML = markup;
 }
 
 async function renderPageByCategory(e) {
-    if (e.target === e.currentTarget) return;
-    const categoryName = e.target.textContent;
-    try {
-        const response = await fetchBooksByCategory(categoryName);
-        galleryList.innerHTML = '';
-        if (response.data.length != 0) {
-            galleryList.style.cssText = 'flex-direction: row; flex-wrap: wrap';
-            renderListByCategory(response.data);
-        } else {
-            showAllCategories();
-        }
-    } catch (error) {
-        console.log(error);
+  if (e.target === e.currentTarget) return;
+  const categoryName = e.target.textContent;
+  try {
+    const response = await fetchBooksByCategory(categoryName);
+    galleryList.innerHTML = '';
+    if (response.data.length != 0) {
+      galleryList.style.cssText = 'flex-direction: row; flex-wrap: wrap';
+      renderListByCategory(response.data);
+    } else {
+      showAllCategories();
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function isCorrectTextLength(text) {
-    const maximumSymbolsCount = 16;
-    return (text.length >= maximumSymbolsCount) ? `${text.slice(0, maximumSymbolsCount)}...` : text;
+  const maximumSymbolsCount = 16;
+  return text.length >= maximumSymbolsCount
+    ? `${text.slice(0, maximumSymbolsCount)}...`
+    : text;
 }
 
 //додаємо обробники подій
-categoryList.addEventListener('click', renderPageByCategory);
+categoryList.addEventListener('click', onClickCategory);
 allCategories.addEventListener('click', showAllCategories);
 
-
-
+async function onClickCategory(e) {
+  await renderPageByCategory(e);
+  console.log('vse');
+}
