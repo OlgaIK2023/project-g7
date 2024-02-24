4859239a2cfc551feec09d120d454395ee3305a7
Grenolf004 <Ghost_Yriy@ukr.net>
2024-02-24 19:36:10 +0200

    test push

--- src/js/modalwindow.js
+++ src/js/modalwindow.js
@@ -1,29 +1,48 @@
 const modal = document.querySelector('.backdrop');
 const closeModalWindow = document.querySelector('.close-modal');
+const galleryList = document.querySelector('.gallery-list');
+
+galleryList.addEventListener('click', (event) => {
+    const item = event.target.closest('.gallery-list-item');
+    if (item) {
+        openModal();
+    }
+});
+
+function openModal() {
+    modal.classList.remove('close');
+    addEventListeners();
+}
+
+function addEventListeners() {
+    closeModalWindow.addEventListener('click', closeModal);
+    modal.addEventListener('click', modalClickHandler);
+    document.addEventListener('keydown', keydownHandler);
+}
 
 function closeModal() {
-  modal.classList.add('close');
-  removeEventListeners();
+    modal.classList.add('close');
+    removeEventListeners();
 }
 
 function removeEventListeners() {
-  closeModalWindow.removeEventListener('click', closeModal);
-  modal.removeEventListener('click', modalClickHandler);
-  document.removeEventListener('keydown', keydownHandler);
+    closeModalWindow.removeEventListener('click', closeModal);
+    modal.removeEventListener('click', modalClickHandler);
+    document.removeEventListener('keydown', keydownHandler);
 }
 
 function modalClickHandler(event) {
-  if (event.target === modal) {
-    closeModal();
-  }
+    if (event.target === modal) {
+        closeModal();
+    }
 }
 
 function keydownHandler(event) {
-  if (event.key === 'Escape' && !modal.classList.contains('close')) {
-    closeModal();
-  }
+    if (event.key === 'Escape' && !modal.classList.contains('close')) {
+        closeModal();
+    }
 }
 
 closeModalWindow.addEventListener('click', closeModal);
-modal.addEventListener('click', modalClickHandler);
-document.addEventListener('keydown', keydownHandler);
+
+
