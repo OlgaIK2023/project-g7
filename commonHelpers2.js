40684fe11e326d57892adbc986c19d2282f35089
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-02-26 22:45:29 +0000

    Deploying to gh-pages from @ OlgaIK2023/project-g7@fdfef6abc11c8122ba0808eb02cb51e229bc9847 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,9 +1,9 @@
-import{l as i,d as h}from"./assets/main-ce1d43cf.js";import"./assets/vendor-0cb09735.js";const e=document.querySelector(".shopping-list"),k=i();function n(a){e.innerHTML="";const c=`<p>This page is empty, add some books and proceed to order.</p>
+import{l as i,d as h}from"./assets/main-f89527ea.js";const e=document.querySelector(".shopping-list"),k=i();function n(t){e.innerHTML="";const c=`<p>This page is empty, add some books and proceed to order.</p>
         <img
         class="no-book"
         src="../img/IMG_9606 1.png"
         alt="Book"
-        />`,r=a.map(({book_image:s,title:o,author:t,_id:l,description:p,list_name:d,amazon_product_url:m,book_uri:g})=>`<li class="one-book">
+        />`,l=t.map(({book_image:s,title:o,author:a,_id:r,description:p,list_name:d,amazon_product_url:m,book_uri:g})=>`<li class="one-book">
             <img
                 class="img-book"
                 src="${s}"
@@ -13,7 +13,7 @@ import{l as i,d as h}from"./assets/main-ce1d43cf.js";import"./assets/vendor-0cb0
                 <div class="up-part">
                 <h2 class="book-name">${o}</h2>
 
-                <button data-id="${l}" class="basket" type="button">
+                <button data-id="${r}" class="basket" type="button">
                     <svg class="trash" width="16" height="16">
                     <use href="./img/icons.svg#icon-trash-031"></use>
                     </svg>
@@ -25,10 +25,10 @@ import{l as i,d as h}from"./assets/main-ce1d43cf.js";import"./assets/vendor-0cb0
                 </p>
 
                 <div class="book-app">
-                <h3 class="name-author">${t}</h3>
+                <h3 class="name-author">${a}</h3>
                 <a href="${m}" target="_blank"><img class="amazon" src="./img/amazon.png" alt="amazon" /></a>
                 <a href="${g}" target="_blank"><img class="apple" src="./img/apple.png" alt="apple" /></a>
                 </div>
             </div>
-            </li>`).join("");e.innerHTML=a.length>0?r:c,document.querySelectorAll(".shopping-list .basket").forEach(s=>{const o=s.getAttribute("data-id");s.addEventListener("click",()=>{h(o);const t=i();n(t)})})}n(k);
+            </li>`).join("");e.innerHTML=t.length>0?l:c,document.querySelectorAll(".shopping-list .basket").forEach(s=>{const o=s.getAttribute("data-id");s.addEventListener("click",()=>{h(o);const a=i();n(a)})})}n(k);
 //# sourceMappingURL=commonHelpers2.js.map
